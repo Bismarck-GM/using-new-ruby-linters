8cf4cfce362700b45fc84065c1d57d679803f078
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-04 17:59:49 -0300

    Added file to try rubocop

---
+++ bubble_sort.rb
@@ -0,0 +1,12 @@
+array = [5, 4, 3, 2, 1]
+
+def bubble_sort (array) 
+  #array.each_with_index{|value, index| p value, index}
+  for i in array do
+    puts array
+  end
+end
+
+p bubble_sort()
+
+que onda perro
