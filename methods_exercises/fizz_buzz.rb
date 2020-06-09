f880047c78e30163a84dc49f24bf7b5b610df58d
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-09 13:26:33 -0300

    Fizz buzz

---
+++ methods_exercises/fizz_buzz.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+puts 'Input a number to iterate the FizzBuzz'
+
+target_number = gets.chomp.to_i
+
+1.upto(target_number) do |number|
+  if (number % 15).zero?
+    puts 'FizzBuzz'
+  elsif (number % 3).zero?
+    puts 'Fizz'
+  elsif (number % 5).zero?
+    puts 'Buzz'
+  else
+    puts number
+  end
+end
